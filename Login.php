49219ec2c67672107649d292eeb853f43391f4f8
sprinkelboy <adja0810@hugs.no>
2016-12-07 13:10:33 +0100

    Update Login.php

--- Login.php
+++ Login.php
@@ -7,18 +7,41 @@
 <link rel="stylesheet" type="text/css" href="css/font-awesome.css" />
 </head>
 <body>
- <div class="container">
-	<img src="user.png">
-    <form>
-	  <div class="form-input">
-	    <input type="text" name="username" placeholder="Enter Username">
+	
+<?php
+if(isset($_POST["username"]) != "")
+{
+	$username=$_POST["username"];
+}else{
+	$username="";
+}
+	
+	if(isset($_POST["password"]) != "")
+{
+	$password=$_POST["password"];
+}else{
+	$password="";
+}
+	
+if($password != "" && $username != "")
+{
+	echo "checking username and password....";
+}else{
+echo "
+ <div class='container'>
+	<img src='user.png'>
+    <form method='POST'>
+	  <div class='form-input'>
+	    <input type='text' name='username' placeholder='Enter Username'>
 	  </div>
-	  <div class="form-input">
-	    <input type="password" name="password" placeholder="Enter Password">
+	  <div class='form-input'>
+	    <input type='password' name='password' placeholder='Enter Password'>
 	  </div>
-	  <input type="submit" name="submit" value="LOGIN" class="btn-login"><br>
-	  <a href="#">Forgot Password?</a>
+	  <input type='submit' name='submit' value='LOGIN' class='btn-login'><br>
+	  <a href='#'>Forgot Password?</a>
 	</form>
-  </div>
+  </div>";
+}
+
 </body>
 </html>
