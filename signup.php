c10d7225ff3edf82432a1479027e54c2dff2c0cd
sprinkelboy <adja0810@hugs.no>
2016-12-07 12:52:48 +0100

    Create signup.php

---
+++ signup.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+<title></title>
+<link rel="stylesheet" type="text/css" href="style.css"  />
+<link rel="stylesheet" type="text/css" href="css/font-awesome.css" />
+</head>
+<body>
+ <div class="container">
+	<img src="user.png">
+    <form>
+	  <div class="form-input">
+	    <input type="text" name="username" placeholder="Enter Username">
+	  </div>
+	  <div class="form-input">
+	    <input type="password" name="password" placeholder="Enter Password">
+	  </div>
+	  <input type="submit" name="submit" value="LOGIN" class="btn-login"><br>
+	  <a href="#">Forgot Password?</a>
+	</form>
+  </div>
+</body>
+</html>
